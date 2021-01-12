0a6576075953b4d97e09698117a50997c3711bee
LouaiKB <loukb1998@gmail.com>
2021-01-12 20:15:02 +0100

    updating the two functions in adn.py

--- adn.py
+++ adn.py
@@ -10,9 +10,21 @@ __author__ = 'Louai KASSA BAGHDOUCHE'
 
 
 def is_valid(adn_str):
-    pass
+    
+    counter = 0
 
+    for i in adn_str.lower(): 
+        if i == 'a' or i == 'c' or i == 'g' or i == 't':
+            counter += 1 
+
+    return True if len(adn_str) == counter else False
 
 def get_valid_adn(prompt='chaîne : '):
-    pass
+    
+    valid = False
+    while not valid:
+        dna = input('Entrez une chaîne valide: ')
+        valid = is_valid(dna)
 
+
+get_valid_adn()
